b82335334fdf9b7a92faf88fff4efef297bfbef6
Nigel John <mytrini@att.net>
2016-06-19 20:42:03 -0400

    Adding BamazonExecutive with connection

---
+++ BamazonExecutive.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+var prompt = require('prompt');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: process.argv[2],
+	database: 'Bamazon'
+});
+
+
+connection.connect(function(err){
+
+	if (err) throw err;
+
+	console.log('connected as id' + connection.threadId + '\n');
+	console.log('============================================');
+	console.log(' Welcome to The Bamazon Executive App!');
+	console.log('Please Select one of the options below!');
+	console.log('============================================'+ '\n\n');
+});
